7737fbcafa2b2edcd9faf50bb330ceebcfc44602
metat <benzz_st@hotmail.com>
2014-06-11 10:12:46 +0700

    Mysimpleapp

---
+++ src/mysimpleapp/MySimpleApp.java
@@ -0,0 +1,21 @@
+
+
+package mysimpleapp;
+
+import sun.applet.Main;
+
+
+public class MySimpleApp {
+    
+    public  void getName(){
+        
+    }
+
+   
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+       
+    }
+    
+}
